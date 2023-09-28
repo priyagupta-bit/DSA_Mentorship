973648ec3e7231e863d68ec8b095df274dcb8e5e
priya <144453330+priyagupta-bit@users.noreply.github.com>
2023-09-28 18:47:55 +0530

    Create A1_Q2.cpp

---
+++ A1_Q2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"enter your marks "<<endl;
+    cin>>n;
+    if(n>90){
+        cout<<"excellent"<<endl;
+    }
+    else if(n>80){
+        cout<<"good"<<endl;
+    }
+    else if(n>70){
+        cout<<"fair"<<endl;
+    }
+    else if(n>60){
+        cout<<"meets expectations"<<endl;
+    }
+    else{
+        cout<<"below par"<<endl;
+    }
+}
